9447dd1edf63459b84c7e85f78f284761c1ccf1b
stone <mr.seventeen98@gmail.com>
2021-10-20 12:04:42 +0800

    修改

--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,8 @@ function tuplify<T extends unknown[]>(...elements: T): T {
   return elements;
 }
 
-export { tuplify };
+function get<T extends object, K extends keyof T>(o: T, name: K) {
+  return o[name];
+}
+
+export { tuplify, get };
